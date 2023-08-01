7b653c7a7a6c242e4f2616a74a64103763b18e53
focain26 <forrest@kollacode.com>
2023-08-01 15:30:16 -0400

    update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'nqiqeq',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      projectId: "nqiqeq"
     },
   },
 });
